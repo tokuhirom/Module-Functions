baef8583fa52afc45592a5f5183c8fae45e8f324
tokuhirom <tokuhirom@gmail.com>
2013-02-07 13:42:11 +0900

    initial import

---
+++ lib/Module/Functions.pm
@@ -0,0 +1,41 @@
+package Module::Functions;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Module::Functions - ...
+
+=head1 SYNOPSIS
+
+  use Module::Functions;
+
+=head1 DESCRIPTION
+
+Module::Functions is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
